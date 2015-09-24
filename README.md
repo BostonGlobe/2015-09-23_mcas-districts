4a1cfe136faf1e457bc21d8cd7ce58a67020e9fb
Gabriel Florit <gabrielflorit@gmail.com>
2015-09-24 14:07:50 -0400

    Select district in dropdown from query string param

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 2015-09-23_mcas-districts
+
+## TODO
+
+- implement two-way communication between parent and child
+- make sure hash is properly encoded to support names with parens inside
+- change hash on dropdown change
+- populate table based on hash
+- style table
+- link districts and index
+- publish
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,54 @@
 'use strict';
 
-var globeIframe = require('globe-iframe-resizer');
+const iframeResizer = require('pym-iframe-resizer');
 
 // This fires when the parent of iframe resizes
 function onPymParentResize(width) {};
-globeIframe(onPymParentResize);
+iframeResizer.resizer(onPymParentResize);
 
-var $ = require('jquery');
-var dataTable = require('datatables');
+// initialize third-party libraries
+const queryString = require('query-string');
+const _ = require('lodash');
+const $ = require('jquery');
+const dataTable = require('datatables');
 $.fn.DataTable = dataTable;
 
-$('table').DataTable();
+// $('table').DataTable();
+
+// get districts data
+const districts = require('./../data/districts.json');
+
+// create list of districts, ordered alpha
+// we will use this later
+const districtsList = _(districts)
+	.pluck('district')
+	.uniq()
+	.sortBy(d=>d)
+	.value();
+
+// get the query string district param
+const parentUrl = queryString.parse(location.search).parentUrl;
+const parentQueryString = queryString.extract(parentUrl);
+let districtParam = queryString.parse(parentQueryString).district;
+
+// if we don't have a district param,
+// choose the first one
+if (!districtParam) {
+	districtParam = districtsList[0];
+}
+
+// construct districts dropdown
+const districtsOptions = _(districtsList)
+	.map(d => {
+
+		const selected = d === districtParam ?
+			'selected' :
+			'';
+
+		return `<option ${selected}>${d}</option>`;
+	})
+	.value().join('');
+
+// populate districts dropdown
+$('.districts-dropdown-wrapper select').html(districtsOptions);
+
